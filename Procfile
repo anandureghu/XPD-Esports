a0b1b7f7eae10d08c2efd94bc31ea3195bee6935
anandureghu <anandukr145@gmail.com>
2021-06-04 18:51:40 +0530

    Pushing on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn projectXPD.wsgi
--- projectXPD/settings.py
+++ projectXPD/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 import os
 from pathlib import Path
 
@@ -134,3 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
